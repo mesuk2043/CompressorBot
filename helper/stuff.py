2dfd4bfec1ad581bfbf63a82ede4dbb88615a4cd
mesuk2043 <85116697+mesuk2043@users.noreply.github.com>
2022-01-31 12:01:23 +0530

    Update stuff.py

--- helper/stuff.py
+++ helper/stuff.py
@@ -60,8 +60,8 @@ async def beck(event):
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-                Button.url("SOURCE CODE", url="github.com/1Danish-00/"),
-                Button.url("DEVELOPER", url="t.me/danish_00"),
+                Button.url("MOVIES GROUP", url="t.me/INDIAHDM0VIES"),
+                Button.url("OWNER", url="t.me/Anshu888o"),
             ],
         ],
     )
