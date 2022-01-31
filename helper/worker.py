9afce33e7563ff9eaac138e88530108e0e5ef063
mesuk2043 <85116697+mesuk2043@users.noreply.github.com>
2022-01-31 11:56:11 +0530

    Update worker.py

--- helper/worker.py
+++ helper/worker.py
@@ -79,7 +79,7 @@ async def encc(e):
                 [Button.inline("CANCEL PROCESS", data=f"skip{wah}")],
             ],
         )
-        cmd = f'ffmpeg -i "{dl}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{out}" -y'
+        cmd = f'ffmpeg -i "{dl}" -map 0 -c:v libx265 -metadata title=krispEncodes -pix_fmt yuv420p -preset fast -s 854x480 -crf 29 -b:v 2M -profile:a aac_he_v2 -c:a libopus -ac 2 -vbr 2 -ab 40k -c:s copy "{out}" -y'
         process = await asyncio.create_subprocess_shell(
             cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
         )
